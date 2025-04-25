b0cd09eafa7af3fcb2934f1afefbb7b1b0cea555
mystick minal <mystick14minal@gmail.com>
2025-04-25 14:28:52 +0545

    Updates

--- src/Navbar/Boxes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './box.css'
-export default function Boxes(props) {
-  return (
-   <div className={props.cls}>
-<img src={props.image} alt='images'/>
-   </div>
-  )
-}
